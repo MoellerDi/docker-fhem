ee0d5f0e2eb18d73c972fe90cd66c9511e281387
Dirk Moeller <dirkmoeller@Dirks-MBP.lan>
2017-12-10 14:07:49 +0100

    new way to restart service dbus

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -3,7 +3,7 @@
 echo "waiting for fhem (15s)"
 sleep 15s
 echo "dbus restarting..."
-/etc/init.d/dbus restart
+service dbus restart
 echo "avahi-daemon starting..."
 service avahi-daemon start
 echo "homebridge starting..."
